413adee38e6e9a6f3934ee03d3430dac467ac764
Jon Ross <jross@atlassian.com>
2018-05-21 10:31:05 -0500

    no really, it should work now

--- .scripts/notifyStride.sh
+++ .scripts/notifyStride.sh
@@ -2,6 +2,6 @@ set +x
 
 curl -X POST \
 -H 'Content-Type: application/json' \
--H 'Authorization: Bearer $STRIDE_TOKEN' \
+-H 'Authorization: Bearer '$STRIDE_TOKEN \
 -d '{"body":{"version":1,"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","text":"'${CHANGE_AUTHOR}' Made a change"}]}]}}' \
---url '$STRIDE_CONVERSATION_URL'
+--url $STRIDE_CONVERSATION_URL
